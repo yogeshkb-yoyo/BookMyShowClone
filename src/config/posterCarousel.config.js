f34946db84a755ed3b355972680afac9a7413c62
yogeshkb-yoyo <yoyokb404@gmail.com>
2021-09-26 01:48:34 +0530

    pro

--- src/config/posterCarousel.config.js
+++ src/config/posterCarousel.config.js
@@ -2,32 +2,32 @@ const settings = {
     infinity: false,
     speed: 500,
     slidesToShow:5,
-    sildesToscroll:4,
+    slidesToscroll:4,
     InitialSlide:0,
     responsive:[
         {
-            breakpoints:1024,
+            breakpoint:1024,
             settings:{
             slidesToShow:3,
-            sildesToscroll:3,
+            slidesToscroll:3,
             infinite:true,
+            },
         },
-    },
-      {
-        breakpoints:600,
-        settings:{
-        slidesToShow:2,
-        sildesToscroll:2,
-        InitialSlide:2,
+        {
+            breakpoint:600,
+            settings:{
+            slidesToShow:2,
+            slidesToscroll:2,
+            InitialSlide:2,
+            },
         },
-     },
-     {
-        breakpoints:480,
-        settings:{
-        slidesToShow:3,
-        sildesToscroll:1,
+        {
+            breakpoint:480,
+            settings:{
+            slidesToShow:3,
+            slidesToscroll:1,
+            },
         },
-     },
     ],
 
 };
